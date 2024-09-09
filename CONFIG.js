a14a17807cdb9599c2447762ef1d86788223e58e
hieu29091996 <96782716+hieu29091996@users.noreply.github.com>
2024-09-09 14:44:03 +0700

    try
    hh

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "Chào Ly, Híu có điều muốn nói. </br> Làm người yêu Híuuu nhé kkk!",
+  descriptionHeader:
+    "Không ĐỒNG Ý cũng phải ĐỒNG Ý leuleu.",
+  buttonYes: "Tớ đồng ý",
+  buttonNo: "Không đồng ý",
+  titleModar: "Cảm ơn cậu.",
+  descriptionModar:
+    "Cảm ơn cậu đã cho tớ một cơ hội để tớ có thể ở bên cạnh, che chở và bảo vệ cậu. </br> Híu biết Híu không được đẹp trai lém kkk (nhưng đi đường gái cũng phải nhìn đó kkk), cũng chẳng lắm tài nhưng H có một trái tim chân thành.</br> Hẹn gặp e vào 1 một ngày đẹp trời, mát mẻ nhó, vì em thích mò kkk</br>  </br> Love you <3",
+};
